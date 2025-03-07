d22b8cf2975dbc474bbdd303e31980ef2c20d03d
benedikt-weyer <bw.development@pm.me>
2025-03-07 14:16:22 +0100

    feat: add registration page with form validation and submission handling

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters."
+    })
+    .email({
+        message: "Username must be a valid email address."
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters."
+    })
+});
+
+const RegisterPage = () => {
+    // form definition
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+    
+    // submit handler definition
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    };
+
+    return (
+        <div className="flex items-center justify-center h-full">
+            <div className="flex flex-col justify-center h-full gap-4">
+
+                <h1 className="text-3xl font-bold">Register</h1>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input placeholder="Username" {...field} />
+                                    </FormControl>
+
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input type="password" placeholder="Password" {...field} />
+                                    </FormControl>
+    
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Register</Button>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
