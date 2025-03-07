2e931c90407441dfdb73b42316d8656c7010b6b9
benedikt-weyer <bw.development@pm.me>
2025-03-07 17:01:28 +0100

    feat: add CORS configuration to SecurityConfig for handling cross-origin requests

--- backend/src/main/java/com/github/benedikt_weyer/streamline_sheduler/security/SecurityConfig.java
+++ backend/src/main/java/com/github/benedikt_weyer/streamline_sheduler/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.github.benedikt_weyer.streamline_sheduler.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -36,5 +41,16 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
