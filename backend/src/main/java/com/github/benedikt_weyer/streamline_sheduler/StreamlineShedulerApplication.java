ca5781cecc7f55a4a5be204c1dc520cf93e8eec9
benedikt-weyer <bw.development@pm.me>
2025-03-07 12:40:35 +0100

    feat: enable JPA auditing in StreamlineShedulerApplication

--- backend/src/main/java/com/github/benedikt_weyer/streamline_sheduler/StreamlineShedulerApplication.java
+++ backend/src/main/java/com/github/benedikt_weyer/streamline_sheduler/StreamlineShedulerApplication.java
@@ -2,8 +2,10 @@ package com.github.benedikt_weyer.streamline_sheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StreamlineShedulerApplication {
 
 	public static void main(String[] args) {
