7d805c48ea964eba2fa81faafa538af69dc767ed
benedikt-weyer <bw.development@pm.me>
2025-03-07 17:04:39 +0100

    feat: add RegistrationResponse class for handling registration feedback

---
+++ backend/src/main/java/com/github/benedikt_weyer/streamline_sheduler/auth/RegistrationResponse.java
@@ -0,0 +1,21 @@
+package com.github.benedikt_weyer.streamline_sheduler.auth;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class RegistrationResponse {
+    
+    private String message;
+
+}
