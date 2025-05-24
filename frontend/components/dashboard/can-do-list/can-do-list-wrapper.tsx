961b9486e18f5b02ad7952bc70de7d7fa9b21410
Benedikt Weyer <bw.development@pm.me>
2025-05-24 17:49:36 +0200

    refactor: update import paths for CanDoListWrapper component

--- frontend/components/dashboard/can-do-list/can-do-list-wrapper.tsx
+++ frontend/components/dashboard/can-do-list/can-do-list-wrapper.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 
 // Dynamically import the CanDoListPage component with no SSR
 const CanDoListPage = dynamic(
-  () => import('../../app/dashboard/can-do-list/page'),
+  () => import('@/app/dashboard/can-do-list/page'),
   { ssr: false }
 );
 
