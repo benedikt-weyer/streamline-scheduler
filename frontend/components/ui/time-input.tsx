6aa477ccabaebe4bfd314d3284b8f0ac4e782647
Benedikt Weyer <bw.development@pm.me>
2025-05-25 14:04:44 +0200

    refactor: update utility imports to use new shadcn-utils module

--- frontend/components/ui/time-input.tsx
+++ frontend/components/ui/time-input.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { ChevronUp, ChevronDown, Clock } from "lucide-react";
 import { Button } from "./button";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/shadcn-utils";
 
 export type QuickTimeOption = {
   value: string;
