60cba14d662c9b76fc33355636fc271ef4fd4338
Benedikt Weyer <bw.development@pm.me>
2025-05-25 15:07:36 +0200

    chore: remove advanced item list component and update exports

--- frontend/components/dashboard/can-do-list/index.ts
+++ frontend/components/dashboard/can-do-list/index.ts
@@ -4,7 +4,6 @@ export { default as AddItemForm } from './add-item-form';
 export { default as EditItemDialog } from './edit-item-dialog';
 export { default as ItemList } from './item-list';
 export { default as ItemListItem } from './item-list-item';
-export { default as ItemListItemAdvanced } from './item-list-item-advanced';
 export { default as ItemActions } from './item-actions';
 export { default as ListControls } from './list-controls';
 export { default as LoadingState } from './loading-state';
