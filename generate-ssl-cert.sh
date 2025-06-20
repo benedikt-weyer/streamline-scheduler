6c16f48c318538792819ef3de6082bb701d89bb9
Benedikt Weyer <bw.development@pm.me>
2025-06-21 00:39:07 +0200

    feat: update SSL certificate generation and Nginx configuration to include additional domain support for home-server.home.arpa

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -8,7 +8,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout ./certs/key.pem \
     -out ./certs/cert.pem \
     -subj "/C=US/ST=State/L=City/O=Organization/OU=OrgUnit/CN=localhost/emailAddress=admin@localhost" \
-    -addext "subjectAltName=DNS:localhost,DNS:127.0.0.1,IP:127.0.0.1"
+    -addext "subjectAltName=DNS:localhost,DNS:127.0.0.1,DNS:home-server.home.arpa,IP:127.0.0.1"
 
 echo "SSL certificates generated successfully!"
 echo "Certificate: ./certs/cert.pem"
